3c05b5b5b31dc0d475e1c613c7999b6116ea16bd
clement <rezperez@gmail.com>
2020-11-12 23:05:47 -0500

    update prettier and eslint to ignore navigation missing from props

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  plugins: ["prettier", "react", "react-native-globals"],
+  parser: 'babel-eslint',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['prettier', 'react', 'react-native-globals'],
   env: {
-    "react-native-globals/all": true,
-    jest: true
+    'react-native-globals/all': true,
+    jest: true,
   },
   overrides: [
     {
-      files: ["**/__tests__/*.js"],
+      files: ['**/__tests__/*.js'],
       env: {
-        jest: true
-      }
-    }
+        jest: true,
+      },
+    },
   ],
   rules: {
-    "prettier/prettier": "error"
+    'prettier/prettier': 'error',
+    'react/prop-types': ['error', { ignore: ['navigation'] }],
   },
   settings: {
     react: {
-      version: "detect"
-    }
-  }
+      version: 'detect',
+    },
+  },
 };
