ee8d6d4f9f3faaeae981d6b18074d5c3cf41c1f4
clement <rezperez@gmail.com>
2020-11-21 14:03:16 -0800

    clean up Bookmarked button

--- src/components/BookmarkButton.js
+++ src/components/BookmarkButton.js
@@ -9,41 +9,44 @@ import { isActivityBookmarked } from '../hooks';
 
 const StyledBookmarkButton = styled.TouchableOpacity`
   background: ${(props) => props.theme.colors.yellowOpacity}
-  width: ${({ small }) => (small ? '30px' : '54px')};
-  height: ${({ small }) => (small ? '30px' : '54px')};
+  width: 54px;
+  height: 54px;
   justify-content: center;
   align-items: center;
-  border-radius: ${({ small }) => (small ? '15px' : '27px')};
+  border-radius: 27px;
 `;
 
-const Bookmark = styled(BookmarkIcon).attrs(() => ({
-  height: '50%',
-  width: '50%',
-  color: 'white',
-}))``;
+const StyledSmallBookmarkButton = styled(StyledBookmarkButton)`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+`;
 
-const Bookmarked = styled(BookmarkedIcon).attrs(() => ({
+const iconParams = {
   height: '50%',
   width: '50%',
   color: 'white',
-}))``;
+};
+const Bookmark = styled(BookmarkIcon).attrs(() => iconParams)``;
+const Bookmarked = styled(BookmarkedIcon).attrs(() => iconParams)``;
 
 function BookmarkButton({ activity, small }) {
   const dispatch = useDispatch();
-
   const selected = isActivityBookmarked(activity);
 
-  const createBookmark = async (activity) => {
+  const createBookmark = async (activity) =>
     await dispatch(postBookmark(activity));
-  };
+
+  const Container = small ? StyledSmallBookmarkButton : StyledBookmarkButton;
+  const Icon = selected ? Bookmarked : Bookmark;
 
   return (
-    <StyledBookmarkButton
+    <Container
       small={small}
       onPress={() => createBookmark(activity)}
     >
-      {selected ? <Bookmarked /> : <Bookmark />}
-    </StyledBookmarkButton>
+      <Icon />
+    </Container>
   );
 }
 
