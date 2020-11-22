2e7075fcfceb5e7548f64c3f67847e971961f4f7
clement <rezperez@gmail.com>
2020-11-22 15:05:02 -0800

    makes react native debugger work with network inspect

--- App.js
+++ App.js
@@ -7,6 +7,17 @@ import theme from './src/style/theme';
 import Navigator from './src/navigator';
 import store from './src/store';
 
+// mecessary for react native debugger inspect network inspect
+global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest;
+global.FormData = global.originalFormData || global.FormData;
+if (window.FETCH_SUPPORT) {
+  window.FETCH_SUPPORT.blob = false;
+} else {
+  global.Blob = global.originalBlob || global.Blob;
+  global.FileReader = global.originalFileReader || global.FileReader;
+}
+// end
+
 const StyledSafeAreaView = styled.SafeAreaView`
   flex: 1;
 `;
