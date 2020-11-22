12d39d13a3db9edc7a52bf1a98026afcad44eaec
clement <rezperez@gmail.com>
2020-11-22 00:26:13 -0800

    modify bookmarks hooks to load on isBookmarked

--- src/hooks/isActivityBookmarked.js
+++ src/hooks/isActivityBookmarked.js
@@ -1,10 +1,19 @@
-import { useSelector } from 'react-redux';
-
-import { isBookmarked } from '../reducers/bookmarks';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  isBookmarked,
+  getBookmarks,
+  fetchBookmarks,
+} from '../reducers/bookmarks';
 
 export default function (activity) {
+  const dispatch = useDispatch();
   const isActivityBookmarked = useSelector((state) =>
     isBookmarked(state, activity),
   );
+  const { isLoading, data } = useSelector(getBookmarks);
+  useEffect(() => {
+    if (!isLoading && !data) dispatch(fetchBookmarks());
+  }, [dispatch]);
   return isActivityBookmarked;
 }
