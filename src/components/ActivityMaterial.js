3ede194e414d94b7a1a274c91136528222ab80ea
clement <rezperez@gmail.com>
2020-11-30 18:39:25 -0800

    adds a check to not crash if null is sent to ActivityMaterial

--- src/components/ActivityMaterial.js
+++ src/components/ActivityMaterial.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text } from 'react-native';
 import PropTypes from 'prop-types';
 
 const ActivityMaterialWrapper = styled.View`
@@ -14,19 +14,20 @@ const ActivityMaterialWrapper = styled.View`
 function ActivityMaterial({ materials }) {
   return (
     <ActivityMaterialWrapper>
-      {materials.map((material) => (
-        <Text key={material.name}>・{material.name}</Text>
-      ))}
+      {materials &&
+        materials.map((material) => (
+          <Text key={material.name}>・{material.name}</Text>
+        ))}
     </ActivityMaterialWrapper>
   );
 }
 
 ActivityMaterial.propTypes = {
-  material: PropTypes.string,
+  materials: PropTypes.array,
 };
 
 ActivityMaterial.defaultProps = {
-  material: 'Default Material',
+  materials: [],
 };
 
 export default ActivityMaterial;
