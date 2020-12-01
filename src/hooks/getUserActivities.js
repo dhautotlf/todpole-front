e0bd6442829ae422dbee58ea7bcb7fa454189637
clement <rezperez@gmail.com>
2020-11-30 17:19:03 -0800

    uncomment

--- src/hooks/getUserActivities.js
+++ src/hooks/getUserActivities.js
@@ -5,9 +5,9 @@ import { getUserActivities, fetchActivities } from '../reducers/activities';
 export default function (user) {
   const dispatch = useDispatch();
   const activities = useSelector((state) => getUserActivities(state, user));
-  // useEffect(() => {
-  //   if (!activities.isLoading && !activities.data) dispatch(fetchActivities());
-  // }, [dispatch]);
+  useEffect(() => {
+    if (!activities.isLoading && !activities.data) dispatch(fetchActivities());
+  }, [dispatch]);
 
   return activities;
 }
